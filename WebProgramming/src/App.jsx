bb2e7cd1028068b3ba6ff24ef8550e5b0a8eafd3
star2t <star2thb@gmail.com>
2023-05-14 14:18:25 +0900

    Feat: openAPI 실습

--- WebProgramming/src/App.jsx
+++ WebProgramming/src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+  const [row, setRow] = useState([]);
+
+  useEffect(() => {
+    console.log('mount or update');
+
+    return () => {
+      console.log('unmount');
+    };
+  });
+
+  useEffect(() => { 
+    console.log('mount only');
+    fetch("http://openAPI.seoul.go.kr:8088/6b736f644a6c6f7436345172576974/json/RealtimeCityAir/1/25/").then(
+      function(res2) {
+        res2.json().then((res3) => {
+          setRow(res3.RealtimeCityAir.row);
+        })
+      }
+    );
+  }, []);
+
+  useEffect(() => { 
+    console.log('update only',row);
+  }, [row]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Vite 시작하기
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th className='th'>지역명</th>
+            <th className='th'>권역구</th>
+            <th className='th'>PM10</th>
+            <th className='th'>PM25</th>
+            <th className='th'>O3</th>
+            <th className='th'>SO2</th>
+            <th className='th'>통합대기지수</th>
+            <th className='th'>미세먼지상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          row.map((gu, index) => {
+            return <tr key={index}>
+              <td>{gu.MSRSTE_NM}</td>
+              <td>{gu.MSRRGN_NM}</td>
+              <td>{gu.PM10}</td>
+              <td>{gu.PM25}</td>
+              <td>{gu.O3}</td>
+              <td>{gu.SO2}</td>
+              <td>{gu.IDEX_MVL}</td>
+              <td>{gu.IDEX_NM}</td>
+              </tr>
+          })
+          }
+        </tbody>
+      </table>
     </>
   )
 }
